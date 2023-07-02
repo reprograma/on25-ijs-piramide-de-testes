e807adc3995075e07aea7c6372b4d1dafa990e39
Roberta <roberta.amaro@ed3.com.br>
2023-07-02 16:14:19 -0300

    refatora primeiro teste para async await

--- para-casa/apiIBGE.test.js
+++ para-casa/apiIBGE.test.js
@@ -5,30 +5,28 @@ const request = require("supertest");
 const apiUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/";
 
 describe("GET estados/ {id} /distritos", () => {
-  it("should return 200 when finds Rio de Janeiro UF", () => {
-    request(apiUrl)
-      .get("estados/33")
-      .expect(200)
-      .then((response) => {
-        expect(response.body.nome).toBe("Rio de Janeiro");
-      });
+  it("should return 200 when finds Rio de Janeiro UF", async () => {
+    const response = await request(apiUrl).get("estados/33");
+    expect(response.status).toBe(200);
+    expect(response.body.nome).toBe("Rio de Janeiro");
   });
   it("should return error 404 trying to find Rio de Janeiro UF", () => {
     request(apiUrl)
-      .get("cidades/33") //altera a rota do get para uma URL que não existe no servidor, retornando assim o erro 404
-      .expect(200)
+      .get("cidades/33")
+      .expect(404) //altera a rota do get para uma URL que não existe no servidor, retornando assim o erro 404
       .then((response) => {
         expect(response.body.nome).toBe("Rio de Janeiro");
       });
   });
 });
 
-describe("GET estados/{ id }/distritos", () => {
-  it("should return all districts of Rio de Janeiro", () => {
-    request(apiUrl)
-      .get("estados/33/distritos")
+describe("GET regioes/{ id }/municipios", () => {
+  it("should return all districts of Rio de Janeiro", async () => {
+    await request(apiUrl)
+      .get("regioes/3/municipios")
       .expect(200)
       .then((response) => {
+        console.log(response.status);
         console.log(response.body);
       });
   });
