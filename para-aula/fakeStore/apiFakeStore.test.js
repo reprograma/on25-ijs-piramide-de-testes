78d85d121548a8fa46dcec923967c7849f2b8aaf
rddavino <rd.davino@gmail.com>
2023-07-05 20:29:49 -0300

    exercicio com api ibge

---
+++ para-aula/fakeStore/apiFakeStore.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest'); //para request http
+const apiUrlFakeStore = "https://fakestoreapi.com/"
+
+describe("GET /products/{$id}", () => {
+    it("should return 200 when finding product 1", () => {
+        request
+    })
+})
